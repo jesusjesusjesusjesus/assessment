b89741bc1cafa06a7a80009efe8ca5237b8a3f9c
jesusjesusjesusjesus <cartero@middleton.school.nz>
2022-05-03 09:35:32 +1200

    welcome and rules v1

---
+++ welcome_and_rules_V1.py
@@ -0,0 +1,4 @@
+# this is the welcome and rules component V1
+# this component welcomes users
+print("*** Welcome to the te reo maori quiz! ***\n"
+      "We hope you have a fantastic time and good luck.")
