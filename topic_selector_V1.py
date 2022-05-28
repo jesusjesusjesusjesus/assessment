288496d8db54fde37c59ea8a7ebfa0b7b40802f5
jesusjesusjesusjesus <cartero@middleton.school.nz>
2022-05-28 23:24:43 +1200

    updated

--- topic_selector_V1.py
+++ topic_selector_V1.py
@@ -1,8 +1,8 @@
 difficulty = 7
 question_difficulty = 0
-topic_selector = 0
+topic_selector = 6
 topic_choice = ""
-topics = ["days of the week", "numbers", "months", "word translations", "common phrase translation", "simple sentence translations"]
+topics = ["days of the week", "numbers", "colours", "word translations", "actions", "months"]
 
 if difficulty <= 2:
     question_difficulty = 1
@@ -24,26 +24,26 @@ elif question_difficulty == 3:
 elif question_difficulty == 4:
     print("These are your available topics", topics[0:])
 
-while topic_selector == 0:
+while topic_selector == 6:
     topic_choice = input("pick a topic: ")
     if topic_choice.lower() == "days of the week":
         print("you chose", topic_choice, "\ngreat choice")
-        topic_selector = 1
+        topic_selector = 0
     elif topic_choice.lower() == "numbers":
         print("you chose", topic_choice, "\ngreat choice")
-        topic_selector = 2
-    elif topic_choice.lower() == "months":
+        topic_selector = 1
+    elif topic_choice.lower() == "colours":
         print("you chose", topic_choice, "\ngreat choice")
-        topic_selector = 3
+        topic_selector = 2
     elif topic_choice.lower() == "word translations":
         print("you chose", topic_choice, "\ngreat choice")
+        topic_selector = 3
+    elif topic_choice.lower() == "actions":
+        print("you chose", topic_choice, "\ngreat choice")
         topic_selector = 4
-    elif topic_choice.lower() == "common phrase translation":
+    elif topic_choice.lower() == "months":
         print("you chose", topic_choice, "\ngreat choice")
         topic_selector = 5
-    elif topic_choice.lower() == "simple sentence translations":
-        print("you chose", topic_choice, "\ngreat choice")
-        topic_selector = 6
     else:
         print("seems you either did not enter a valid answer or you spelt it incorrectly\nlets try again")
-        topic_selector = 0
+        topic_selector = 6
