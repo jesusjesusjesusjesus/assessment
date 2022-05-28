c884c434a29b509d68439037ebf8a7fe0869c588
jesusjesusjesusjesus <cartero@middleton.school.nz>
2022-05-28 22:58:27 +1200

    update

--- questions_data_V1.py
+++ questions_data_V1.py
@@ -1,21 +1,24 @@
-topics = ["days of the week", "numbers", "months", "word translations", "common phrase translation",
-          "simple sentence translations"]
+topics = ["days of the week", "numbers", "colours", "word translations", "actions", "months"]
 days_of_the_week_maori = ["rahina", "ratu", "raapa", "rapare", "ramere", "rahoroi", "ratapu"]
 days_of_the_week_english = ["monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"]
-numbers_maori = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "iwa", "tekau", "rua tekau", "rima tekau",
+numbers_maori = ["tahi", "rua", "toru", "wha", "rima", "ono", "whitu", "waru", "iwa", "tekau", "rua tekau", "rima tekau",
                  "kotahi rau"]
 numbers_english = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "twenty",
-                   "one hundred"]
+                   "fifty", "one hundred"]
 months_maori = ["Kohi-tātea", "Hui-tanguru", "Poutū-te-rangi", "Paenga-whāwhā", "Haratua", "Pipiri", "Hōngongoi",
                 "Here-turi-kōkā", "Mahuru", "Whiringa-ā-nuku", "Whiringa-ā-rangi", "Hakihea"]
 months_english = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october",
                   "november", "december"]
 word_translations_maori = ["aroha", "awa", "haka", "iwi", "kai", "karakia", "marae", "maunga", "moana", "tamariki", "tapu"]
 word_translations_english = ["love", "river", "dance", "river", "food", "prayer", "meeting house", "mountain", "ocean", "children", "sacred"]
-common_phrase_translation_maori = []
+colours_maori = ["ma", "whero", "kowhai", "kakariki", "pango", "mawhero", "kahurangi", "kiwikiwi", "karaka", "tawa"]
+colours_english = ["white", "red", "yellow", "green", "black", "pink", "blue", "grey", "orange", "purple"]
+actions_maori = ["oma", "peke", "kikoi", "e tu", "e noho", "pakipaki", "hurihuri", "korero", "whakarongo", "tangi"]
+actions_english = ["run", "jump", "walking", "stand up", "sit down", "clap", "spin", "talk", "listen", "cry"]
 
-
-
+colours = [colours_maori, colours_english]
+months = [months_maori, months_english]
 days_of_the_week = [days_of_the_week_maori, days_of_the_week_english]
 numbers = [numbers_maori, numbers_english]
-word_translations = [word_translations_english, word_translations_maori]
+word_translations = [word_translations_maori, word_translations_english]
+actions = [actions_maori, actions_english]
